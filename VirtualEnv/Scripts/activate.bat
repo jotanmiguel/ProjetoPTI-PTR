488ed25f3aa8d45d7d18a2625175899482b556c5
DiogoEsteves7 <diogoferreiraesteves@hotmail.com>
2023-05-03 19:53:20 +0100

    add front-end loja e produto
    :+1:

--- VirtualEnv/Scripts/activate.bat
+++ VirtualEnv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\migue\Documents\GitHub\ProjetoPTI-PTR\VirtualEnv
+set VIRTUAL_ENV=C:\Users\diogo\Desktop\D.E\3ºAno\2ºSemestre\PTI.PTR\ProjetoPTI-PTR\VirtualEnv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(VirtualEnv) 
 
 :END
 if defined _OLD_CODEPAGE (
